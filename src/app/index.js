d41b18acf2ce00cf31e46a56b337e10024a85775
Lubomir <lyubomirlugovii97@gmail.com>
2021-06-15 13:02:16 +0300

    minor server start fix

--- src/app/index.js
+++ src/app/index.js
@@ -21,45 +21,14 @@ module.exports = (host, port) => new Promise((res, rej) => {
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
 
-  app.set('host', host);
-  app.set('port', port);
+  app.listen(port, host, (err) => {
+    if (err) {
+      console.error(`Cannot start due to ${err.message}`);
 
-  app.listen(3000, 'localhost', () => {
+      return rej(err);
+    }
+
     console.log(`Listening server on ${host}:${port}`);
-
     res(app);
-  });
-
-  app.on('error', (error) => {
-    if (!isStarted) {
-      isStarted = true;
-
-      rej(error);
-
-      return;
-    }
-
-    if (error.syscall !== 'listen') {
-      throw error;
-    }
-
-    const bind = typeof port === 'string'
-      ? `Pipe ${port}`
-      : `Port ${port}`;
-
-    // handle specific listen errors with friendly messages
-    switch (error.code) {
-      case 'EACCES':
-        console.error(`${bind} requires elevated privileges`);
-        process.exit(1);
-        break;
-      case 'EADDRINUSE':
-        console.error(`${bind} is already in use`);
-
-        process.exit(1);
-        break;
-      default:
-        throw error;
-    }
   });
 });
